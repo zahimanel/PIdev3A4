24c7e4db606b9bb83523fe150bf445d6370ea872
zahimanel <zahimanel@yahoo.fr>
2014-02-18 20:42:13 +0100

    2eme modification membre1

---
+++ PIdev3A4Mem/src/pidev3a4mem/PIdev3A4.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pidev3a4mem;
+
+/**
+ *
+ * @author user
+ */
+public class PIdev3A4 {
+
+    private int age;
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
